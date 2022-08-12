0fb3a35cc07d76c8bd7308f4850e1811f4c3487d
Andrea Spiga <drespriz@gmail.com>
2022-08-12 14:33:17 +0200

    adds hero section

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -2,6 +2,8 @@ export default function Hero() {
   return (
     <div className="hero-wrapper">
       <img src="app/assets/airbnb.png" width="50px" alt="aribnb" />
+      <h1 className="hero-header">Online Experiences</h1>
+      <p className="hero-copy">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
     </div>
   )
 }
