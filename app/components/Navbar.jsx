b76a504c496523cfe892902501e6a4cf8f39d741
Andrea Spiga <drespriz@gmail.com>
2022-08-12 12:50:20 +0200

    initial commit

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,12 @@
+// these are default settings
+
+export default function Navbar() {
+  return (
+    <nav className="nav">
+      <div className="logo">
+        <img src="app/assets/logo.svg" alt="logo" />
+        <h2>asdasda</h2>
+      </div>
+    </nav>
+  )
+}
