c9a7c9fcee48aadddcb30e74f6c11f22cc82692b
Andrea Spiga <drespriz@gmail.com>
2022-08-13 20:53:06 +0200

    refactors badge logic

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -1,7 +1,17 @@
 export default function Card(props) {
+  let badgeText
+  let cardClass
+  if (props.spots === 0) {
+    badgeText = "SOLD OUT !"
+    cardClass = "card-openSpots-red"
+  } else {
+    badgeText = `OPEN SPOTS: ${props.spots}`
+    cardClass = "card-openSpots"
+  }
+
   return (
     <div className="card">
-      <div className={props.spots===0 && "card-openSpots-red" || props.spots > 0 && "card-openSpots" }>{props.spots === 0 && "SOLD OUT !" || props.spots > 0 && "OPEN SPOTS: " + props.spots}</div>
+      <div className={cardClass}> {badgeText} </div>
       <img className="card-img" src={props.img} alt={props.alt} />
       <div className="card-text-wrapper">
         <p className="card-rating">⭐ {props.rating} ({props.reviews}) • {props.place}</p>
@@ -12,13 +22,3 @@ export default function Card(props) {
     </div>
   )
 }
-const cards = document.querySelectorAll(".cards")
-console.log(cards)
-
-cards.forEach(c => {
-  console.log(c)
-  if (c.innerText.includes("S"))
-    c.classList.remove('card-openSpots')
-    c.classList.add('card-openSpots-red')
-  }
-)
